d501ecb43bc9c2f9bfb848ee87dc52a69c564012
Benjamin <bmurphy30@my.bcit.ca>
2018-12-10 22:41:04 -0800

    THIS IS A TEST

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const hbs = require("hbs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper("message", (text) => {
+	return text.toUpperCase();
+});
+
+/*
+app.use((request, response, next) => {
+	response.render("maintenance.hbs", {
+		title: "Maintenance page"
+	});
+});
+*/
+
+app.get('/', (request, response) => {
+	//response.send("<h1>Hello Express!<h1>");
+	response.send({
+		name: "Your name",
+		school: [
+			"BCIT",
+			"SFU",
+			"UBC"
+			]
+	})
+});
+
+app.get("/info", (request, response) => {
+	response.render("about.hbs", {
+		title: "About page",
+		year: new Date().getFullYear(),
+		welcome: "Hello!"
+	});
+});
+
+app.get("/404", (request, response) => {
+	response.send({
+		error: "Page not found"
+	})
+});
+
+app.listen(8080, () => {
+	console.log("Server is up on the port 8080");
+});
